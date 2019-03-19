26be0b71698747a82234cb47de95b427314a30b2
M999RHV <ruslan.hassonov@gmail.com>
2019-03-19 16:43:26 +0100

    none

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,5 @@
 package sample;
 
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -9,7 +8,6 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
-import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -37,16 +35,30 @@ public class Controller {
                 lbl_outputPath.setText(path.toString());
                 DirectoryStream<Path> stream = Files.newDirectoryStream(path);
 
-                for (Path p : stream){
-                    listOfTypes.add(p.getFileName().toString());
-                }
 
-                lv_listOfTypes.setItems(listOfTypes);
+
+                /*for (Path p : stream){
+                    String fileName = p.getFileName().toString();
+                    listOfTypes.add(getExtensionOfFile(fileName));
+                }
+                lv_listOfTypes.setItems(listOfTypes);*/
 
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    //Get extension from file name
+    private String getExtensionOfFile(String s){
+        int i = s.lastIndexOf(".");
+
+        if (i > 0){
+            return s.substring(i+1);
+        }
+
+        return ""; //empty extension
     }
 
 
