42ef21e7aada5cc92c07d6cc9d7c0c2d07135218
M999RHV <ruslan.hassonov@gmail.com>
2019-03-19 15:43:56 +0100

    none

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,14 @@
 package sample;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
-import javax.swing.table.TableColumn;
-import java.awt.*;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -13,19 +18,37 @@ import java.nio.file.Paths;
 
 public class Controller {
     @FXML
-    private TextField tf_fileDirectoryName;
-
-    @FXML
-    private Button btn_submit;
+    private TextField tf_directoryName;
 
     @FXML
     private Label lbl_outputPath;
 
     @FXML
-    private TableColumn tbc_fileType;
+    private ListView<String> lv_listOfTypes;
 
     @FXML
-    private TableColumn tbc_amount;
+    void submitButtonPressed(ActionEvent event){
+
+        Path path = Paths.get(tf_directoryName.getText());
+        final ObservableList<String> listOfTypes = FXCollections.observableArrayList();
+
+        try {
+            if (Files.isDirectory(path)){
+                lbl_outputPath.setText(path.toString());
+                DirectoryStream<Path> stream = Files.newDirectoryStream(path);
+
+                for (Path p : stream){
+                    listOfTypes.add(p.getFileName().toString());
+                }
+
+                lv_listOfTypes.setItems(listOfTypes);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 
